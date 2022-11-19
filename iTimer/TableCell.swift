3ab5c310e5345036f0d35690de1106aff1cc638d
Ltati <proksi_n@mail.ru>
2022-11-20 01:12:51 +0300

    Timer for IOS

---
+++ iTimer/TableCell.swift
@@ -0,0 +1,14 @@
+//
+//  TableCell.swift
+//  iTimer
+//
+//  Created by user on 19/11/22.
+//
+
+import UIKit
+
+class TableCell: UITableViewCell {
+
+    @IBOutlet weak var tableLabel: UILabel!
+    
+}
